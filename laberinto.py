eb39828a3d009e772ba9bb6d5ffa615a7ccd86e0
Julianj-mtz <a01254258@tec.mx>
2023-08-11 15:43:00 -0700

    Laberinto

--- laberinto.py
+++ laberinto.py
@@ -1,31 +1,71 @@
 
+# José Julián Martínez Romero A01254258
+# Act laberinto
 
 import pygame, sys
 from pygame.locals import *
 pygame.init()
-DisplaySurf = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('AAAAAAAAAA!')
 
-ColorCuadrado01 = (100,255,100)
-ColorCuadrado02 = (255,190, 255)
+# Pantalla
+DisplaySurf = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Laberinto')
+
+# Color
+pink = (255,190, 255)
  
+# Coordenadas de los muros
 muros = [
-	()
+	(0,0,0,600),
+    (50,0,600,0),
+    (0,600,600,600),
+    (600,0,600,550),
+    (50,0,50,400),
+    (50,550,50,600),
+    (50,550,300,550),
+    (300,550,300,100),
+    (50,500,250,500),
+    (250,500,250,450),
+    (50,450,250,450),
+    (100,50,350,50),
+    (100,400,250,400),
+    (100,100,300,100),
+    (100,400,100,100),
+    (150,350,150,150),
+    (200,400,200,150),
+    (250,350,250,150),
+    (350,50,350,50),
+    (350,50,350,450),
+    (350,500,450,500),
+    (350,600,350,550),
+    (400,550,400,600),
+    (400,450,400,250),
+    (400,200,400,100),
+    (400,50,550,50),
+    (400,100,550,100),
+    (450,150,550,150),
+    (450,200,500,200),
+    (400,250,550,250),
+    (450,200,450,150),
+    (550,250,550,150),
+    (550,100,550,50),
+	(450,500,450,300),
+	(450,300,550,300),
+	(500,350,500,600),
+	(400,550,450,550),
+	(550,550,550,300),
+	(550,550,600,550),    
 ]
 
-def armarMuro():
+# Función que arma el laberinto
+def armarLaberinto():
 	for muro in muros:
-		pygame.draw.line()
+		pygame.draw.line(DisplaySurf, pink, (muro[0], muro[1]), (muro[2], muro[3]), 5)
 		
 while True:
 	for event in pygame.event.get():
-		
-		cuadrado01 = pygame.draw.rect( #X1, Y1, Ancho, Largo
-		DisplaySurf, ColorCuadrado01, (0,0,600,600))
-		cuadrado02 = pygame.draw.rect( #X1, Y1, Ancho, Largo
-		DisplaySurf, ColorCuadrado02, (110,110,100,100))
-
+		armarLaberinto()                # Llamar a la función
 		if event.type == QUIT:
 			pygame.quit()
-			sys.exit()
+			sys.exit()			
 	pygame.display.update()
+	
